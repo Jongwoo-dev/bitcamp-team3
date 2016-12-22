f4b2e6136cbcceb699bc2768251f235bf04a708f
suhee <sue.parksuhee@gmail.com>
2016-12-22 19:01:51 +0900

    도메인 생성

---
+++ src/main/java/bitcamp/java89/ems2/domain/Project.java
@@ -0,0 +1,41 @@
+package bitcamp.java89.ems2.domain;
+
+public class Project {
+
+  protected int projectNo;
+  protected String title;
+  protected String contents;
+  protected String startDate;
+  protected String endDate;
+  public int getProjectNo() {
+    return projectNo;
+  }
+  public void setProjectNo(int projectNo) {
+    this.projectNo = projectNo;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getContents() {
+    return contents;
+  }
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
+  public String getStartDate() {
+    return startDate;
+  }
+  public void setStartDate(String startDate) {
+    this.startDate = startDate;
+  }
+  public String getEndDate() {
+    return endDate;
+  }
+  public void setEndDate(String endDate) {
+    this.endDate = endDate;
+  }
+  
+}
