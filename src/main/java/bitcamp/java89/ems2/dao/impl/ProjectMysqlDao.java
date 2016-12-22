1b561e7564ab5297eac1ec1e3d8c49e70ed77b26
suhee <sue.parksuhee@gmail.com>
2016-12-22 19:19:09 +0900

    ProjetMysqlDao.getList 생성

---
+++ src/main/java/bitcamp/java89/ems2/dao/impl/ProjectMysqlDao.java
@@ -0,0 +1,43 @@
+package bitcamp.java89.ems2.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import bitcamp.java89.ems2.dao.ProjectDao;
+import bitcamp.java89.ems2.domain.Project;
+import bitcamp.java89.ems2.util.DataSource;
+
+public class ProjectMysqlDao implements ProjectDao {
+  DataSource ds;
+  
+  public void setDataSource(DataSource ds) {
+    this.ds = ds;
+  }
+  
+  public ArrayList<Project> getList() throws Exception {
+    ArrayList<Project> list = new ArrayList<>();
+    Connection con = ds.getConnection();
+    
+    try (
+      PreparedStatement stmt = con.prepareStatement(
+          "select titl,sdt,edt,pjno from proj left outer join proj_memb on proj.pjno=proj_memb.pjno");
+      ResultSet rs = stmt.executeQuery(); ){
+      
+      while (rs.next()) {
+        Project project = new Project();
+        project.setTitle(rs.getString("titl"));
+        project.setStartDate(rs.getString("sdt"));
+        project.setEndDate(rs.getString("edt"));
+        project.setProjectNo(Integer.parseInt(rs.getString("pjno")));
+        
+        list.add(project);
+      }
+    } finally {
+      ds.returnConnection(con);
+    }
+    return list;
+  }
+  
+}
